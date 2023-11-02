b044f70baf793fe73adbe50c7ed16c34602b3331
Nick Genovese <ngenovese@pepperdash.com>
2023-11-02 15:38:07 -0400

    feat: ported over essentials rooms to plugin

---
+++ src/EssentialsNDisplayRoomBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Config;
+
+namespace PDT.Plugins.Essentials.Rooms
+{
+    /// <summary>
+    /// Base class for rooms with more than a single display
+    /// </summary>
+    public abstract class EssentialsNDisplayRoomBase : EssentialsRoomBase, IHasMultipleDisplays
+    {
+        //public event SourceInfoChangeHandler CurrentSingleSourceChange;
+
+        public Dictionary<eSourceListItemDestinationTypes, IRoutingSinkWithSwitching> Displays { get; protected set;}
+
+        public EssentialsNDisplayRoomBase(DeviceConfig config)
+            : base (config)
+        {
+            Displays = new Dictionary<eSourceListItemDestinationTypes, IRoutingSinkWithSwitching>();
+
+        }
+    }
+}
---
+++ src/IEssentialsHuddleSpaceRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PDT.Plugins.Essentials.Rooms.Config;
+using PepperDash.Essentials.Core;
+
+namespace PDT.Plugins.Essentials.Rooms
+{
+    public interface IEssentialsHuddleSpaceRoom : IEssentialsRoom, IHasCurrentSourceInfoChange, IRunRouteAction, IHasDefaultDisplay, IHasCurrentVolumeControls, IRoomOccupancy,
+        IEmergency, IMicrophonePrivacy
+    {
+        bool ExcludeFromGlobalFunctions { get; }
+
+        void RunRouteAction(string routeKey);
+
+        EssentialsHuddleRoomPropertiesConfig PropertiesConfig { get; }
+
+        IBasicVolumeControls CurrentVolumeControls { get; }
+
+        event EventHandler<VolumeDeviceChangeEventArgs> CurrentVolumeDeviceChange;
+    }
+}
